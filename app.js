f911744dd9a230bb47cbbeba50c260a9901a2457
zhwang <516275397@qq.com>
2016-05-30 09:25:23 +0800

    增加html显示页面，js和css资源文件

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 var express = require('express');
 var app = express();
+var port = 3000;
+var path = require('path');
+
+app.use(express.static(path.join(__dirname, '/static')));
+app.use(function (req, res) {
+    res.sendfile(path.join(__dirname, '/static/index.html'));
+});
+
+var server = app.listen(port, function (err) {
+    if (err) {
+        return console.error("app start fail");
+    }
+
+    console.log("app start success and port " + port);
+});
