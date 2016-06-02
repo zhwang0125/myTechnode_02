606aa72fe54f23575566bf42e44b9e2cc751cfc8
zhwang <516275397@qq.com>
2016-06-02 10:57:07 +0800

    添加用户加入广播室聊天事件

--- app.js
+++ app.js
@@ -38,11 +38,29 @@ var server = app.listen(port, function (err) {
 
 var socketIO = require('socket.io');
 var io = socketIO.listen(server);
+var User = require('./controllers/user').User;
 var SYSTEM = {
     name: 'technode_机器人',
     avatarUrl: 'http://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Robot_icon.svg/220px-Robot_icon.svg.png'
-}
+};
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
+    var _userId = socket.request.session._userId;
 
+    User.online(_userId, function (err, user) {
+        if (err) {
+            socket.emit('err', {
+                msg: err
+            });
+        }
+        else {
+            socket.broadcast.emit('users.add', user);
+            socket.broadcast.emit('messages.add', {
+                content: user.name + '进入了聊天室',
+                creator: SYSTEM,
+                createAt: new Date()
+            });
+        }
+
+    });
 });
